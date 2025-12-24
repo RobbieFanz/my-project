a544fe18f56818944752419cdeaf7efb9ca102d7
RobbieFanz <fanzuttir@gmail.com>
2025-12-23 21:20:16 -0500

    improved

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -8,19 +8,20 @@ export function Projects(){
     return(<>
         <div className="pt-35">
         <h1 className="text-6xl md:text-7xl lg:text-8xl font-bold text-white tracking-tighter text-center">Projects</h1>
-        <SpotlightCard className="grid grid-cols-1 text-center xl:grid-cols-2 xl:text-left m-15 gap-2 max-w-6xl justify-self-center justify-items-center" spotlightColor="rgba(255, 255, 255, 0.11)">
+        <SpotlightCard className="grid grid-cols-1 text-center xl:grid-cols-2 xl:text-left m-15 gap-4 max-w-6xl justify-self-center justify-items-center" spotlightColor="rgba(255, 255, 255, 0.11)">
             <img className="col-span-1 rounded-md self-center" src={projectImg} alt="project" />
             <p className="col-span-1 self-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum maiores dicta ipsa nihil dolores doloribus aliquid autem blanditiis enim eaque reprehenderit ullam, in architecto at doloremque ipsum, laudantium ad? Distinctio.
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt sapiente a nostrum accusamus accusantium modi iste architecto iusto est. Delectus eligendi temporibus officiis dignissimos praesentium, voluptatum recusandae eaque adipisci atque!
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam illo nam nihil ullam, maxime perferendis ut laboriosam atque aut consequuntur at quae eius, tempora odit ipsa? Assumenda, dolorem? Dolores
             </p>
         </SpotlightCard>
-        <SpotlightCard className="grid grid-cols-1 text-center xl:grid-cols-2 xl:text-left m-15 gap-2 max-w-6xl justify-self-center justify-items-center" spotlightColor="rgba(255, 255, 255, 0.11)">
-            <img className="col-span-1 rounded-md self-center" src={projectImg} alt="project" />
+        <SpotlightCard className="grid grid-cols-1 text-center xl:grid-cols-2 xl:text-left m-15 gap-4 max-w-6xl justify-self-center justify-items-center" spotlightColor="rgba(255, 255, 255, 0.11)">
+            
             <p className="col-span-1 self-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum maiores dicta ipsa nihil dolores doloribus aliquid autem blanditiis enim eaque reprehenderit ullam, in architecto at doloremque ipsum, laudantium ad? Distinctio.
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt sapiente a nostrum accusamus accusantium modi iste architecto iusto est. Delectus eligendi temporibus officiis dignissimos praesentium, voluptatum recusandae eaque adipisci atque!
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam illo nam nihil ullam, maxime perferendis ut laboriosam atque aut consequuntur at quae eius, tempora odit ipsa? Assumenda, dolorem? Dolores
             </p>
+            <img className="col-span-1 rounded-md self-center" src={projectImg} alt="project" />
         </SpotlightCard>
         </div>
    
